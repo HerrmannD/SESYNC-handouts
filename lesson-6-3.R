23cdb05ce930a00f9179620041afac0ebc8fd5fa
Dustin Herrmann <Dustin Herrmann>
2016-09-15 16:22:07 -0400

    End of day

--- lesson-6-3.R
+++ lesson-6-3.R
@@ -1,6 +1,6 @@
 # Libraries
-library(...)
-library(...)
+library(ggplot2)
+library(dplyr)
 
 # Data
 species <- read.csv("data/species.csv", stringsAsFactors = FALSE)
@@ -9,15 +9,35 @@ surveys <- read.csv("data/surveys.csv", na.strings = "", stringsAsFactors = FALS
 # User Interface
 in1 <- selectInput("pick_species",
                    label = "Pick a species",
-                   choices = unique(species[["species_id"]]))
-...
-tab <- tabPanel("Species", in1, ...)
+                   choices = unique(species["species_id"]))
+side <- sidebarPanel(h2("Options", align="center"), in1)
+out1 <- textOutput("species_name")
+tab1 <- tabPanel("Plot",
+                 plotOutput("species_plot"))
+tab2 <- tabPanel("Data",
+                 dataTableOutput("species_table"))                 
+out2 <- tabsetPanel(tab1, tab2)
+main <- mainPanel(out1, out2)
+tab <- tabPanel("Species",
+                sidebarLayout(side, main))
 ui <- navbarPage(title = "Portal Project", tab)
 
-# Server
 server <- function(input, output) {
+  output[["species_name"]] <- renderText(
+    species %>%
+      filter(species_id == input[["pick_species"]]) %>%
+      select(genus, species) %>%
+      paste(collapse = ' ')
+  )
   output[["species_plot"]] <- renderPlot(
-    ...
+    surveys %>%
+      filter(species_id == input[["pick_species"]]) %>%
+      ggplot(aes(year)) +
+      geom_bar()
+  )
+  output[["species_table"]] <- renderDataTable(
+    surveys %>%
+      filter(species_id == input[["pick_species"]])
   )
 }
 
